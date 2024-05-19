d24b14255b0e61fae93339c91fea2ed5e7523cd0
Draco9990 <51196669+Draco9990@users.noreply.github.com>
2024-05-20 00:44:47 +0100

    Cleanup

--- src/main/java/dLib/util/MapNodeManipulator.java
+++ src/main/java/dLib/util/MapNodeManipulator.java
@@ -9,6 +9,33 @@ import com.megacrit.cardcrawl.map.MapRoomNode;
 import java.util.ArrayList;
 
 public class MapNodeManipulator {
+    //region Variables
+
+    //endregion Variables
+
+    //region Methods
+
+    //region Node Connecting
+    public static void connectNodes(MapRoomNode from, MapRoomNode to){
+        MapEdge connectingEdge = new MapEdge(from.x, from.y, from.offsetX, from.offsetY, to.x, to.y, to.offsetX, to.offsetY, false);
+        from.addEdge(connectingEdge);
+        addParent(to, from);
+        addChild(from, to, connectingEdge);
+    }
+
+    private static void addParent(MapRoomNode node, MapRoomNode parent){
+        Patches.NodeFields.parents.get(node).add(parent);
+    }
+
+    private static void addChild(MapRoomNode node, MapRoomNode child, MapEdge connectingEdge){
+        Patches.NodeFields.children.get(node).add(new NodeEdgePair(child, connectingEdge));
+    }
+
+    //endregion Node Connecting
+
+    //endregion Methods
+
+    //region Node Edge Pair
     public static class NodeEdgePair{
         public MapRoomNode node;
         public MapEdge edge;
@@ -18,42 +45,22 @@ public class MapNodeManipulator {
             this.edge = edge;
         }
     }
+    //endregion Node Edge Pair
 
-    @SpirePatch2(clz = MapRoomNode.class, method = "<class>")
-    public static class NodeFields{
-        public static SpireField<Boolean> isCustom = new SpireField<>(() -> false);
-
-        public static SpireField<ArrayList<MapRoomNode>> parents = new SpireField<>(ArrayList::new);
-        public static SpireField<ArrayList<NodeEdgePair>> children = new SpireField<>(ArrayList::new);
-    }
-
-    public static void connectNodes(MapRoomNode from, MapRoomNode to){
-        MapEdge connectingEdge = new MapEdge(from.x, from.y, from.offsetX, from.offsetY, to.x, to.y, to.offsetX, to.offsetY, false);
-        from.addEdge(connectingEdge);
-        addParent(to, from);
-        addChild(from, to, connectingEdge);
-    }
-
-    private static void addParent(MapRoomNode node, MapRoomNode parent){
-        NodeFields.isCustom.set(node, true);
-        NodeFields.parents.get(node).add(parent);
-    }
-
-    private static void addChild(MapRoomNode node, MapRoomNode child, MapEdge connectingEdge){
-        NodeFields.isCustom.set(node, true);
-        NodeFields.children.get(node).add(new NodeEdgePair(child, connectingEdge));
-    }
-
-    /** Patches */
+    //region Patches
     public static class Patches{
+        @SpirePatch2(clz = MapRoomNode.class, method = "<class>")
+        public static class NodeFields{
+            public static SpireField<ArrayList<MapRoomNode>> parents = new SpireField<>(ArrayList::new);
+            public static SpireField<ArrayList<NodeEdgePair>> children = new SpireField<>(ArrayList::new);
+        }
+
         @SpirePatch2(clz = MapRoomNode.class, method = "isConnectedTo")
         public static class IsConnectedToPatch{
             public static SpireReturn<Boolean> Prefix(MapRoomNode __instance, MapRoomNode node){
-                if(NodeFields.isCustom.get(__instance)){
-                    for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
-                        if(nodeEdgePair.node.equals(node)){
-                            return SpireReturn.Return(true);
-                        }
+                for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
+                    if(nodeEdgePair.node.equals(node)){
+                        return SpireReturn.Return(true);
                     }
                 }
 
@@ -63,11 +70,9 @@ public class MapNodeManipulator {
         @SpirePatch2(clz = MapRoomNode.class, method = "wingedIsConnectedTo")
         public static class WingedIsConnectedToPatch{
             public static SpireReturn<Boolean> Prefix(MapRoomNode __instance, MapRoomNode node){
-                if(NodeFields.isCustom.get(__instance)){
-                    for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
-                        if(nodeEdgePair.node.equals(node)){
-                            return SpireReturn.Return(true);
-                        }
+                for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
+                    if(nodeEdgePair.node.equals(node)){
+                        return SpireReturn.Return(true);
                     }
                 }
 
@@ -78,11 +83,9 @@ public class MapNodeManipulator {
         @SpirePatch2(clz = MapRoomNode.class, method = "getEdgeConnectedTo")
         public static class GetEdgeConnectedToPatch{
             public static SpireReturn<MapEdge> Prefix(MapRoomNode __instance, MapRoomNode node){
-                if(NodeFields.isCustom.get(__instance)){
-                    for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
-                        if(nodeEdgePair.node.equals(node)){
-                            return SpireReturn.Return(nodeEdgePair.edge);
-                        }
+                for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
+                    if(nodeEdgePair.node.equals(node)){
+                        return SpireReturn.Return(nodeEdgePair.edge);
                     }
                 }
 
@@ -93,46 +96,40 @@ public class MapNodeManipulator {
         @SpirePatch2(clz = MapRoomNode.class, method = "leftNodeAvailable")
         public static class leftNodeAvailablePatch{
             public static SpireReturn<Boolean> Prefix(MapRoomNode __instance){
-                if(NodeFields.isCustom.get(__instance)){
-                    for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
-                        if(nodeEdgePair.node.x < __instance.x){
-                            return SpireReturn.Return(true);
-                        }
+                for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
+                    if(nodeEdgePair.node.x < __instance.x){
+                        return SpireReturn.Return(true);
                     }
                 }
 
                 return SpireReturn.Continue();
             }
         }
-
-        @SpirePatch2(clz = MapRoomNode.class, method = "centerNodeAvailable")
-        public static class centerNodeAvailablePatch{
-            public static SpireReturn<Boolean> Prefix(MapRoomNode __instance){
-                if(NodeFields.isCustom.get(__instance)){
-                    for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
-                        if(nodeEdgePair.node.x == __instance.x){
-                            return SpireReturn.Return(true);
-                        }
-                    }
-                }
-
-                return SpireReturn.Continue();
-            }
-        }
-
         @SpirePatch2(clz = MapRoomNode.class, method = "rightNodeAvailable")
         public static class rightNodeAvailablePatch{
             public static SpireReturn<Boolean> Prefix(MapRoomNode __instance){
-                if(NodeFields.isCustom.get(__instance)){
-                    for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
-                        if(nodeEdgePair.node.x > __instance.x){
-                            return SpireReturn.Return(true);
-                        }
+                for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
+                    if(nodeEdgePair.node.x > __instance.x){
+                        return SpireReturn.Return(true);
                     }
                 }
 
                 return SpireReturn.Continue();
             }
         }
+        @SpirePatch2(clz = MapRoomNode.class, method = "centerNodeAvailable")
+        public static class centerNodeAvailablePatch{
+            public static SpireReturn<Boolean> Prefix(MapRoomNode __instance){
+                for(NodeEdgePair nodeEdgePair : NodeFields.children.get(__instance)){
+                    if(nodeEdgePair.node.x == __instance.x){
+                        return SpireReturn.Return(true);
+                    }
+                }
+
+                return SpireReturn.Continue();
+            }
+        }
+
     }
+    //endregion Patches
 }
