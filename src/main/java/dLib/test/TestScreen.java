06710de03757107874c13dccc545b05d4ed9a9b8
Draco9990 <51196669+Draco9990@users.noreply.github.com>
2024-09-19 12:22:06 +0100

    Cleanup

--- src/main/java/dLib/test/TestScreen.java
+++ src/main/java/dLib/test/TestScreen.java
@@ -1,10 +1,8 @@
 package dLib.test;
 
+import dLib.ui.Alignment;
 import dLib.ui.animations.exit.UIAnimation_SlideOutDown;
-import dLib.ui.elements.prefabs.Button;
-import dLib.ui.elements.prefabs.TextButton;
-import dLib.ui.elements.prefabs.VerticalGridBox;
-import dLib.ui.elements.prefabs.VerticalListBox;
+import dLib.ui.elements.prefabs.*;
 import dLib.ui.screens.AbstractScreen;
 
 import java.util.ArrayList;
@@ -16,13 +14,25 @@ public class TestScreen extends AbstractScreen {
     public TestScreen(){
         addGenericBackground();
 
-        ArrayList<String> test = new ArrayList<>();
+        TextBox box = new TextBox("HI HI HI HI", 100, 100, 300, 300).setWrap(true);
+        box.setVerticalAlignment(Alignment.VerticalAlignment.CENTER);
+        box.setMarginPercX(0).setMarginPercY(0);
+        addChildNCS(box);
 
-        for(int i = 0; i < 100; i++){
-            test.add("Test " + i);
-        }
-
-        addChildNCS(new VerticalListBox<String>(100, 100, 1920-200, 1080-200).setItems(test));
+        addChildNCS(new Button(700, 1080-700, 50, 50).addOnLeftClickConsumer(new Runnable() {
+            @Override
+            public void run() {
+                if(box.getVerticalAlignment() == Alignment.VerticalAlignment.TOP){
+                    box.setVerticalAlignment(Alignment.VerticalAlignment.CENTER);
+                }
+                else if(box.getVerticalAlignment() == Alignment.VerticalAlignment.CENTER){
+                    box.setVerticalAlignment(Alignment.VerticalAlignment.BOTTOM);
+                }
+                else{
+                    box.setVerticalAlignment(Alignment.VerticalAlignment.TOP);
+                }
+            }
+        }));
     }
 
     @Override
--- src/main/java/dLib/ui/elements/prefabs/TextBox.java
+++ src/main/java/dLib/ui/elements/prefabs/TextBox.java
@@ -223,12 +223,13 @@ public class TextBox extends Hoverable {
             else if(alignment.horizontalAlignment == Alignment.HorizontalAlignment.CENTER) align = Align.center;
             else if(alignment.horizontalAlignment == Alignment.HorizontalAlignment.RIGHT) align = Align.right;
 
-            if(alignment.verticalAlignment == Alignment.VerticalAlignment.TOP) renderY += (int) (halfHeight * 1.5f);
-            else if(alignment.verticalAlignment == Alignment.VerticalAlignment.CENTER) renderY += (int) halfHeight;
-            else if(alignment.verticalAlignment == Alignment.VerticalAlignment.BOTTOM) renderY += (int) (halfHeight * 0.5f);
-
             FontHelper.layout.setText(getFontForRender(), text, Color.WHITE, renderWidth * Settings.xScale, align, true);
-            getFontForRender().draw(sb, text, renderX * Settings.xScale, (renderY + FontHelper.layout.height / 2f) * Settings.yScale, renderWidth * Settings.xScale, align, true);
+
+            if(alignment.verticalAlignment == Alignment.VerticalAlignment.TOP) renderY = renderY + renderHeight;
+            else if(alignment.verticalAlignment == Alignment.VerticalAlignment.CENTER) renderY = renderY + renderHeight / 2 + (int) (FontHelper.layout.height / 2 / Settings.yScale);
+            else if(alignment.verticalAlignment == Alignment.VerticalAlignment.BOTTOM) renderY = renderY + (int) (FontHelper.layout.height / Settings.yScale);
+
+            getFontForRender().draw(sb, text, renderX * Settings.xScale, renderY * Settings.yScale, renderWidth * Settings.xScale, align, true);
             getFontForRender().getData().setScale(1.0F);
         }
 
