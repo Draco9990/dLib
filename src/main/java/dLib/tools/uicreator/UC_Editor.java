7324bde3085864384d3c8ef0d97561f784fb21eb
Draco9990 <51196669+Draco9990@users.noreply.github.com>
2024-12-04 21:28:00 +0000

    UCEditor pt1

--- src/main/java/dLib/tools/uicreator/UC_Editor.java
+++ src/main/java/dLib/tools/uicreator/UC_Editor.java
@@ -1,6 +1,20 @@
 package dLib.tools.uicreator;
 
+import com.badlogic.gdx.graphics.Color;
 import dLib.ui.elements.UIElement;
+import dLib.ui.elements.implementations.Renderable;
+import dLib.ui.themes.UITheme;
+import dLib.util.ui.dimensions.Dim;
+import dLib.util.ui.position.Pos;
 
-public class UC_Editor extends UIElement {
+public class UC_Editor extends Renderable {
+
+    public UC_Editor(){
+        super(UITheme.whitePixel, Pos.perc(0), Pos.perc(0), Dim.fill(), Dim.fill());
+        setRenderColor(Color.valueOf("#151515FF"));
+    }
+
+    //region Subclasses
+
+    //region Subclasses
 }
